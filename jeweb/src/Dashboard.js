3220d904541b47a7a3f52f12a30b387a0b4d8b9b
giem <dlrlaud0806@gmail.com>
2022-04-30 00:38:00 +0900

    add dashboard page

---
+++ jeweb/src/Dashboard.js
@@ -0,0 +1,25 @@
+import { CardHeading } from 'react-bootstrap-icons';
+import { Link } from 'react-router-dom'
+
+function Dashboard() {
+  return (
+    <div className="container">
+        <div className='p-4 m-4 fs-3 fw-bolder lh-lg text-center'> 
+                <h2 class="section-heading text-uppercase">서비스</h2>
+        </div>
+        
+        <div class="col-md-4">
+            <span class="fa-stack fa-4x">
+            <Link to="./Board/Board">
+                <CardHeading width='200' height='200'/>
+                </Link>
+            </span>
+            <h4 class="my-3">블로그 리뷰 리스트</h4>
+        </div>
+        
+    </div>
+  );
+}
+
+
+export default Dashboard;
--- jeweb/src/index.js
+++ jeweb/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import Board from './Board/Board';
+// import Board from './Board/Board';
+import Dashboard from './Dashboard';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Board />
+    <BrowserRouter>
+      <Dashboard />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
